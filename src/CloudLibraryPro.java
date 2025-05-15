5e4e401da6e742fdcdcfc7cc15d56dc04a9bffe6
Virati Akira Nandhan Reddy <viratiaki53@gmail.com>
2025-05-15 10:46:02 +0530

    Made My First Program In Java

---
+++ src/CloudLibraryPro.java
@@ -0,0 +1,7 @@
+public class CloudLibraryPro {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("I'm Virati Akira Nandhan Reddy");
+        System.out.println("My First Program In Java");
+    }
+}
